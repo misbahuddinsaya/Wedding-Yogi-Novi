9dd59053ad2f99966ba164ed04548dafff3f2db3
misbahuddinsaya <misbahuddinsaya26@gmail.com>
2024-08-11 22:51:15 +0700

    Perubahan JS

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ import { util } from './util.js';
 import { like } from './like.js';
 import { theme } from './theme.js';
 import { audio } from './audio.js';
-import { comment } from './comment.js';
+
 import { progress } from './progress.js';
 import { pagination } from './pagination.js';
 
